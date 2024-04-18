85ff68c610fa72cb6619e9736ea923e281760e6b
Luke Storry <luke.storry@letsdothis.com>
2024-04-18 12:28:19 +0100

    add font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,13 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.astro"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Berkshire Swash", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 };
