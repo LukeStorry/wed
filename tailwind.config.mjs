8b31e9466cdbd0dc23c9b8bca85a09ff3c64eaf1
Luke Storry <luke.storry@letsdothis.com>
2024-04-18 14:47:45 +0100

    add animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,24 @@ export default {
       fontFamily: {
         sans: ["Berkshire Swash", ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        spinTease: {
+          "0%, 100%": { transform: "rotateY(0deg)" },
+          "50%": { transform: "rotateY(20deg) skewY(1deg)" },
+        },
+        spinOut: {
+          "0%": { transform: "rotateY(0deg)" },
+          "100%": { transform: "rotateY(90deg) skewY(6deg)" },
+        },
+        spinIn: {
+          "0%": { transform: "rotateY(270deg) skewY(-6deg)" },
+          "100%": { transform: "rotateY(360deg)" },
+        },
+      },
+      animation: {
+        "spin-tease": "spinTease 1s ease-in-out 2",
+      },
     },
   },
+  safelist: ["animate-[spinIn]", "animate-[spinOut]"], // keyframes used by astro page transisions, so not classNamed
 };
