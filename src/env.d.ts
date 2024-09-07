43798f0000b05f5eb7fd3551c3783415e96a8c38
Luke Storry <luke.storry@faculty.ai>
2024-09-07 17:47:48 +0100

    add zod

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,5 +3,8 @@
 interface ImportMeta {
   readonly env: {
     readonly PASSWORD: string;
+    readonly SPREADSHEET_ID: string;
+    readonly GOOGLE_SERVICE_ACCOUNT_EMAIL: string;
+    readonly GOOGLE_PRIVATE_KEY: string;
   };
 }
